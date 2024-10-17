5e5bfef54ee03767c532e5227655a3a855e7baed
Bosdrew <dyandrew024@gmail.com>
2024-10-17 16:04:12 +0800

    Create program1.cpp

---
+++ images/program1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+     string name;
+      cout<<"Welcome to C++ language Programming"; 
+      cout<<"\nGood morning pal!, what is your name? "; 
+      cout<<"Hi " + name + ", Welcome to C++ Programming Class.\n"; 
+      }

5a0c40f14d4615d7c4b1046ee959bf1a04b00878
Bosdrew <dyandrew024@gmail.com>
2024-11-28 14:31:31 +0800

    Update program1.cpp

--- images/program1.cpp
+++ images/program1.cpp
@@ -5,5 +5,6 @@ int main() {
      string name;
       cout<<"Welcome to C++ language Programming"; 
       cout<<"\nGood morning pal!, what is your name? "; 
+      cin>>name;
       cout<<"Hi " + name + ", Welcome to C++ Programming Class.\n"; 
       }
